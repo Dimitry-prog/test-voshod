6f186c3350aa7878dfa104705a1ab4c6ec627fa6
Dmitry <kfifa3@gmail.com>
2024-01-28 16:36:04 +0300

    finish

---
+++ hooks/use-custom-params.ts
@@ -0,0 +1,19 @@
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export const useCustomParams = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const params = new URLSearchParams(searchParams);
+
+  const handleReplaceParams = (params: URLSearchParams) => {
+    replace(`${pathname}?${params}`, { scroll: false })
+  }
+
+  return {
+    searchParams,
+    pathname,
+    params,
+    replace: handleReplaceParams
+  }
+}
